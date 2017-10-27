a034ee835280c491e76b28057c4c245914ecfa3c
Maximilian Bischoff <maximilianbischoff@web.de>
2017-10-27 16:29:21 +0200

    fix: Malicious code vulnerability (Mutable static field) suggested by FindBugs

--- app/src/main/java/de/hdm/dp/bd/chronophage/database/TaskDatabaseInMemoryMock.java
+++ app/src/main/java/de/hdm/dp/bd/chronophage/database/TaskDatabaseInMemoryMock.java
@@ -3,7 +3,7 @@ package de.hdm.dp.bd.chronophage.database;
 import de.hdm.dp.bd.chronophage.models.TaskListModel;
 
 public class TaskDatabaseInMemoryMock {
-    public static TaskListModel taskListModel = new TaskListModel();
+    public static final TaskListModel taskListModel = new TaskListModel();
 
     private TaskDatabaseInMemoryMock() {
     }
