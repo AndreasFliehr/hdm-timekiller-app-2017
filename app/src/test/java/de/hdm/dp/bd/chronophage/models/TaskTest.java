4cb5323962ec57c7a8b7928720c36a8cb5c4d942
Andreas Fliehr <afliehr@gmx.de>
2017-11-10 17:24:22 +0100

    refactor: Added whitebox tests for task class

---
+++ app/src/test/java/de/hdm/dp/bd/chronophage/models/TaskTest.java
@@ -0,0 +1,49 @@
+package de.hdm.dp.bd.chronophage.models;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskTest {
+    Task task;
+
+    @Before
+    public void setUp() {
+        task = new Task(0,"Test");
+    }
+
+    @Test
+    public void start_newTask_executes() {
+        task.start();
+    }
+
+    @Test (expected = Exception.class)
+    public void start_taskAlreadyStarted_throwsException() {
+        task.start();
+        task.start();
+    }
+
+    @Test
+    public void stop_taskAlreadyStarted_executes() {
+        task.start();
+        task.stop();
+    }
+
+    @Test (expected = Exception.class)
+    public void stop_taskNotYetStarted_throwsException() {
+        task.stop();
+    }
+
+    @Test
+    public void getOverallDuration_newTask_returnsZero() {
+        Assert.assertEquals(0,task.getOverallDuration());
+    }
+
+    @Test
+    public void getOverallDuration_taskHasRecord_returnsMoreThanZero() throws Exception {
+        task.start();
+        wait(1000);
+        task.stop();
+        Assert.assertTrue(0 < task.getOverallDuration());
+    }
+}
