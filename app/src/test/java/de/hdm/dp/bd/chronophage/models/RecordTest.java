f9f41b3926ba90dbdaee720acae6fd6ef6903112
Maximilian Bischoff <maximilianbischoff@web.de>
2017-11-10 17:10:14 +0100

    refactor: Removed superfluous throws declarations

--- app/src/test/java/de/hdm/dp/bd/chronophage/models/RecordTest.java
+++ app/src/test/java/de/hdm/dp/bd/chronophage/models/RecordTest.java
@@ -3,7 +3,6 @@ package de.hdm.dp.bd.chronophage.models;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class RecordTest {
     Record record;
@@ -14,54 +13,54 @@ public class RecordTest {
     }
 
     @Test
-    public void start_newRecord_executes() throws Exception {
+    public void start_newRecord_executes() {
         record.start();
     }
 
     @Test(expected = Exception.class)
-    public void start_recordAlreadyStarted_throwsException() throws Exception {
+    public void start_recordAlreadyStarted_throwsException() {
         record.start();
         record.start();
     }
 
     @Test(expected = Exception.class)
-    public void start_recordAlreadyStopped_throwsException() throws Exception {
+    public void start_recordAlreadyStopped_throwsException() {
         record.start();
         record.stop();
         record.start();
     }
 
     @Test(expected = Exception.class)
-    public void stop_newRecord_throwsException() throws Exception {
+    public void stop_newRecord_throwsException() {
         record.stop();
     }
 
     @Test
-    public void stop_recordStarted_executes() throws Exception {
+    public void stop_recordStarted_executes() {
         record.start();
         record.stop();
     }
 
     @Test(expected = Exception.class)
-    public void stop_recordStopped_throwsException() throws Exception {
+    public void stop_recordStopped_throwsException() {
         record.start();
         record.stop();
         record.stop();
     }
 
     @Test(expected = Exception.class)
-    public void getDuration_newRecord_throwsException() throws Exception {
+    public void getDuration_newRecord_throwsException() {
         record.getDuration();
     }
 
     @Test(expected = Exception.class)
-    public void getDuration_recordStartedButNotStopped_throwsException() throws Exception {
+    public void getDuration_recordStartedButNotStopped_throwsException() {
         record.start();
         record.getDuration();
     }
 
     @Test
-    public void getDuration_recordStartedThenStopped_returnsDuration() throws Exception {
+    public void getDuration_recordStartedThenStopped_returnsDuration() {
         record.start();
         record.stop();
         record.getDuration();
