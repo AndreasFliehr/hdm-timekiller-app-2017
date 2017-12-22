1db54ac926610bdfa091d07a98e449e2e242e4a0
Simon Schuster <simon.schuster@meetnow.eu>
2017-12-22 15:27:01 +0100

    feat: DatePicker-GUI for filtering the pie chart added

--- app/src/main/java/de/hdm/dp/bd/chronophage/EvalActivity.java
+++ app/src/main/java/de/hdm/dp/bd/chronophage/EvalActivity.java
@@ -1,6 +1,12 @@
 package de.hdm.dp.bd.chronophage;
 
 import android.os.Bundle;
+import android.app.DatePickerDialog;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
@@ -8,7 +14,9 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import de.hdm.dp.bd.chronophage.database.TaskDatabaseInMemoryMock;
 import de.hdm.dp.bd.chronophage.models.Task;
@@ -19,6 +27,8 @@ import de.hdm.dp.bd.chronophage.models.Task;
  * "Evaluation" im Drawer-Menü aufgerufen wird
  */
 public class EvalActivity extends CommonActivity {
+    private EditText startDateEditText;
+    private EditText endDateEditText;
 
     /**
      * diese Methode muss nicht verändert werden, sie baut das Kuchendiagramm auf
@@ -28,6 +38,10 @@ public class EvalActivity extends CommonActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eval);
         createNavigationBars();
+
+        // Date filter
+        startDateEditText = (EditText) findViewById(R.id.startDateEditText);
+        endDateEditText = (EditText) findViewById(R.id.endDateEditText);
 
         //Content of the pie chart
         PieChart pieChart = (PieChart) findViewById(R.id.chart);
@@ -41,6 +55,66 @@ public class EvalActivity extends CommonActivity {
         dataset.setColors(ColorTemplate.COLORFUL_COLORS); // set the color
         dataset.setValueTextSize(16);
 
+        setupDatePicker();
+    }
+
+
+    private void setupDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+
+        // implement the date picker dialog
+        final DatePickerDialog startDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                //TODO hier wird die Reaktion auf as vom Benutzer eeingegebene Datum reagiert
+                //Hier: Anzeige des Datums im Textfeld
+                openDatePicker(year, month, day, startDateEditText);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar
+            .DAY_OF_MONTH));
+
+        final DatePickerDialog endDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                //TODO hier wird die Reaktion auf as vom Benutzer eeingegebene Datum reagiert
+                //Hier: Anzeige des Datums im Textfeld
+                openDatePicker(year, month, day, endDateEditText);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar
+            .DAY_OF_MONTH));
+
+        // prevent showing keyboard
+        startDateEditText.setInputType(InputType.TYPE_NULL);
+        endDateEditText.setInputType(InputType.TYPE_NULL);
+
+        startDateEditText.setText("Start date");
+        endDateEditText.setText("End date");
+
+        // register from edit text listener
+        startDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("EvalActivity", "onclick gefeuert");
+
+                startDatePicker.show();
+            }
+        });
+
+        endDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("EvalActivity", "onclick gefeuert");
+
+                endDatePicker.show();
+            }
+        });
+    }
+
+    private void openDatePicker(int year, int month, int day, EditText dateEditText) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar date = Calendar.getInstance();
+        date.set(year, month, day);
+        dateEditText.setText(simpleDateFormat.format(date.getTime()));
     }
 
     /**
