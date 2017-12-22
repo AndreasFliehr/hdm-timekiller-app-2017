bf5e394e11e7dd43199bd021c40acf550af503d7
Maximilian Bischoff <maximilianbischoff@web.de>
2017-12-22 15:56:27 +0100

    feat: added test for new methods

--- app/src/test/java/de/hdm/dp/bd/chronophage/models/TaskListTest.java
+++ app/src/test/java/de/hdm/dp/bd/chronophage/models/TaskListTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
@@ -34,6 +36,23 @@ public class TaskListTest {
     @Test
     public void isTaskActive_newTaskList_isTrue() throws Exception {
         assertTrue(taskList.isTaskActive(taskMock));
+    }
+
+    @Test
+    public void getAllTasksWithRecords_oneTaskWithRecordsOneWithout_returnsListWithTaskWithRecord() {
+        //test setup
+        TaskList test = new TaskList();
+        Task withRecords = Mockito.mock(Task.class);
+        doReturn(1L).when(withRecords).getOverallDuration();
+        Task withoutRecords = Mockito.mock(Task.class);
+        doReturn(0L).when(withoutRecords).getOverallDuration();
+        test.getAllTasks().add(withoutRecords);
+        test.getAllTasks().add(withRecords);
+        //execute test
+        final List<Task> tasksWithRecords = test.getAllTasksWithRecords();
+        assertNotNull(tasksWithRecords);
+        assertTrue(tasksWithRecords.contains(withRecords));
+        assertFalse(tasksWithRecords.contains(withoutRecords));
     }
 
 }
